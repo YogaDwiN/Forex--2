53a35d7e5745032c590731a66ab921877e7d56f6
Yoga Dwi N <yogadwin03@gmail.com>
2025-06-13 19:47:57 +0700

    first commit

---
+++ app/src/main/java/com/yogadwin/forex2/ForexViewHolder.java
@@ -0,0 +1,18 @@
+package com.yogadwin.forex2;
+
+import android.widget.TextView;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ForexViewHolder extends RecyclerView.ViewHolder {
+    TextView kodeTextView, kursTextView;
+
+    public ForexViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        kodeTextView = itemView.findViewById(R.id.kodeTextView);
+        kursTextView = itemView.findViewById(R.id.kursTextView);
+    }
+}
